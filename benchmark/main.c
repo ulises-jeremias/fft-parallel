7a09962544ab88e6f84cd15ea8db9c1b65f31c14
ulises-jeremias <ulisescf.24@gmail.com>
2023-02-21 13:50:55 -0300

    Updated benchmark

--- benchmark/main.c
+++ benchmark/main.c
@@ -55,7 +55,14 @@ run_serial(size_t N, size_t N1, size_t N2, size_t TIMES)
 	serial_avgs[input_size_idx] = serial_avg;
 	serial_std_devs[input_size_idx] = sqrt((serial_sum - serial_avg * serial_avg) / TIMES);
 
-	(void)result;
+        // print a LaTeX table row containing the values \emph{N} & \emph{Tiempo}
+        // for the Tiempo columns use 3 decimal places
+        // for the N column use 0 decimal places
+        // use the & symbol to separate the columns
+        // use the \\ symbol to separate the rows
+        print_log("%ld & %.3f $\\pm$ %.3f \\\\\n", N, serial_avg, serial_std_devs[input_size_idx]);
+
+        (void)result;
 
 	free(input);
 }
@@ -96,9 +103,14 @@ run_pthread_benchmark(size_t N, size_t N1, size_t N2, size_t NUM_THREADS, size_t
 	efficiency = speedup / NUM_THREADS;
 	total_overhead = NUM_THREADS * parallel_avg - serial_avgs[input_size_idx];
 
-	print_log("Pthread: %f (+/-) %f\nSpeedup: %f\nEfficiency: %f\nOverhead: %f\n", parallel_avg, parallel_std_dev, speedup, efficiency, total_overhead);
+        // print a LaTeX table row containing the values \emph{N} & \emph{Tiempo} & \emph{Speedup} & \emph{Eficiencia} & \emph{Overhead}
+        // for the Tiempo, Speedup, Eficiencia and Overhead columns use 3 decimal places
+        // for the N column use 0 decimal places
+        // use the & symbol to separate the columns
+        // use the \\ symbol to separate the rows
+        print_log("%ld & %.3f $\\pm$ %.3f & %.3f & %.3f & %.3f \\\\\n", N, parallel_avg, parallel_std_dev, speedup, efficiency, total_overhead);
 
-	(void)result;
+        (void)result;
 
 	free(input);
 }
@@ -138,9 +150,14 @@ run_openmp_benchmark(size_t N, size_t N1, size_t N2, size_t NUM_THREADS, size_t 
 	efficiency = speedup / NUM_THREADS;
         total_overhead = NUM_THREADS * parallel_avg - serial_avgs[input_size_idx];
 
-        print_log("OpenMP: %f (+/-) %f\nSpeedup: %f\nEfficiency: %f\nOverhead: %f\n", parallel_avg, parallel_std_dev, speedup, efficiency, total_overhead);
+        // print a LaTeX table row containing the values \emph{N} & \emph{Tiempo} & \emph{Speedup} & \emph{Eficiencia} & \emph{Overhead}
+        // for the Tiempo, Speedup, Eficiencia and Overhead columns use 3 decimal places
+        // for the N column use 0 decimal places
+        // use the & symbol to separate the columns
+        // use the \\ symbol to separate the rows
+        print_log("%ld & %.3f $\\pm$ %.3f & %.3f & %.3f & %.3f \\\\\n", N, parallel_avg, parallel_std_dev, speedup, efficiency, total_overhead);
 
-	(void)result;
+        (void)result;
 
 	free(input);
 }
@@ -162,30 +179,47 @@ main(int argc, char **argv)
 	{
 		N = INPUTS_N[input_size_idx];
 		N2 = N / N1;
-		print_log("Initializing serial benchmark with N = %ld...\n", N);
 		run_serial(N, N1, N2, times);
-		print_log("Done initializing serial benchmark with N = %ld...\n", N);
 	}
 
 	print_log("Done initializing serial benchmarks...\n\n");
 
 	for (i = 0; i < ARRAY_SIZE(NUM_THREADS); i++)
 	{
+                num_threads = NUM_THREADS[i];
+
+                // print a LaTex row such us \rowcolor{black!45} \multicolumn{5}{l}{\color{white}N hilos}
+                print_log("\\rowcolor{black!45} \\multicolumn{5}{l}{\\color{white}%ld hilos} \\\\\n", num_threads);
+
 		for (input_size_idx = 0; input_size_idx < ARRAY_SIZE(INPUTS_N); input_size_idx++)
 		{
 			N = INPUTS_N[input_size_idx];
 			N2 = N / N1;
-			num_threads = NUM_THREADS[i];
-
-			print_log("Benchmarks with N = %ld, N1 = %ld, N2 = %ld, NUM_THREADS = %ld, TIMES = %ld:\n", N, N1, N2, num_threads, times);
-			print_log("Serial: %f (+/-) %f\n", serial_avgs[input_size_idx], serial_std_devs[input_size_idx]);
 
 			run_pthread_benchmark(N, N1, N2, num_threads, times);
-			run_openmp_benchmark(N, N1, N2, num_threads, times);
-
-			print_log("\n");
 		}
+
+                print_log("\n");
 	}
 
-	return 0;
+        for (i = 0; i < ARRAY_SIZE(NUM_THREADS); i++)
+        {
+                num_threads = NUM_THREADS[i];
+
+                // print a LaTex row such us \rowcolor{black!45} \multicolumn{5}{l}{\color{white}N hilos}
+                print_log("\\rowcolor{black!45} \\multicolumn{5}{l}{\\color{white}%ld hilos} \\\\\n", num_threads);
+
+                for (input_size_idx = 0; input_size_idx < ARRAY_SIZE(INPUTS_N); input_size_idx++)
+                {
+                        N = INPUTS_N[input_size_idx];
+                        N2 = N / N1;
+
+                        run_openmp_benchmark(N, N1, N2, num_threads, times);
+
+                }
+
+                print_log("\n");
+        }
+
+        return 0;
 }
